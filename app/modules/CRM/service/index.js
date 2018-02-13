47a719a56d6843f5b3e5cdd8de5c56748c2e847c
salexey1990 <amsirotkin1990@gmail.com>
2018-02-13 22:35:14 +0300

    correct statuses

--- app/modules/CRM/service/index.js
+++ app/modules/CRM/service/index.js
@@ -6,23 +6,33 @@ const lodash = require('lodash');
 
 module.exports = class ServiceEW {
     async getLeads(params) {
-        const find = {
-            AdditionalFields: {
-                af_45: 'true'
-            }
-        }
-        const leads = await wrapper.searchEntity('Leads', find);
+
+        const leads = await wrapper.getEntity('Leads');
+        const statArr = [
+            'dcec2644-c590-11e5-903f-6466b305cde0',
+            'e14f81b0-bf7a-11e5-bda1-6466b305cde0',
+            '73a9fde6-c041-11e5-8ef2-6466b305cde0',
+            'bc14312b-c041-11e5-8ef2-6466b305cde0',
+            '90c78af1-65ac-49e3-8e32-88f23b32ef03',
+            '037a8b0c-be7f-11e5-9558-6466b305cde0',
+            '94f0b2b4-c041-11e5-8ef2-6466b305cde0'
+        ]
         let res = {
             statuses: [
-                {id: 1, name: 'Новый'},
-                {id: 2, name: 'В работе'}
+                {id: 'dcec2644-c590-11e5-903f-6466b305cde0', name: 'Отгружен'},
+                {id: 'e14f81b0-bf7a-11e5-bda1-6466b305cde0', name: 'Есть интерес'},
+                {id: '73a9fde6-c041-11e5-8ef2-6466b305cde0', name: 'Счёт'},
+                {id: 'bc14312b-c041-11e5-8ef2-6466b305cde0', name: 'Отвал'},
+                {id: '90c78af1-65ac-49e3-8e32-88f23b32ef03', name: 'Новый'},
+                {id: '037a8b0c-be7f-11e5-9558-6466b305cde0', name: 'Квалифицирован'},
+                {id: '94f0b2b4-c041-11e5-8ef2-6466b305cde0', name: 'Продажа'}
             ]
         }
         const orders = lodash.compact(leads.Data.map((order) => {
             let day = order.ItemChanged.substring(0,2);
             let month = order.ItemChanged.substring(3,5);
             let rest = order.ItemChanged.substring(6);
-            if (new Date(params.date*1000) <= new Date(`${month}.${day}.${rest}`)) {
+            if ((new Date(params.date*1000) <= new Date(`${month}.${day}.${rest}`)) && (lodash.includes(statArr, order.StateEn))) {
                 return {
                     id: order.ItemGUID,
                     name: order.FileAs,
