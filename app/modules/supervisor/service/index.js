5de800c3f02b8d9753db80ee9cf6e7f478ffd028
Alex <amsirotkin1990@gmail.com>
2018-06-05 21:27:30 +0300

    resurce types

--- app/modules/supervisor/service/index.js
+++ app/modules/supervisor/service/index.js
@@ -29,9 +29,35 @@ module.exports = class ServiceSV {
                 order_id : params.id
             }
         }
-        const resource = JSON.stringify(params)
+        let resource;
+        if (params.title.indexOf('Звонок') != -1) {
+            resource = '8800'
+        } else if (params.title.indexOf('Пойманный лид') != -1) {
+            resource = 'Leadhunt'
+        } else if (params.data && JSON.parse(params.data).marker == 'УТП') {
+            resource = 'First'
+        } else if (params.data && JSON.parse(params.data).marker == 'Заказать звонок') {
+            resource = 'Recall'
+        } else if (params.data && JSON.parse(params.data).marker && JSON.parse(params.data).marker.indexOf('Карточка товара, кнопка купить') != -1) {
+            resource = 'Product'
+        } else if (params.data && JSON.parse(params.data).marker == 'Главная страница, форма захвата по email') {
+            resource = 'Getemail'
+        } else if (params.data && JSON.parse(params.data).marker == 'Главная страница, география поставки') {
+            resource = 'Dist'
+        } else if (params.data && JSON.parse(params.data).marker && JSON.parse(params.data).marker.indexOf('Карточка товара, форма') != -1) {
+            resource = 'Help'
+        }  else if (params.data && JSON.parse(params.data).marker == 'Каталог') {
+            resource = 'Help'
+        } else if (params.data && JSON.parse(params.data).marker && JSON.parse(params.data).marker.indexOf('Выполняемы работы, форма') != -1) {
+            resource = 'Inst'
+        } else if (params.data && JSON.parse(params.data).marker == 'Контакты') {
+            resource = 'Contacts'
+        } else {
+            resource = 'Email'
+        }
         const newLead = {
             date: params.created_date,
+            title: params.title || 'не известен',
             resource: resource || 'не известен',
             comment: params.text || null,
             name: params.name || null,
